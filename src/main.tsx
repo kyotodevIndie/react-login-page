22cc633f10ad1ec53faa5c55ca9a11e925877658
Thallys Morais <kyotodevindie@gmail.com>
2023-12-10 19:57:29 -0300

    feat: add query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './styles/index.sass'
+import 'react-toastify/dist/ReactToastify.css'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './services/queryClient'
+import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
