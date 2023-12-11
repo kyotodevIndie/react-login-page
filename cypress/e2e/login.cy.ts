83e583b4195561d4d54a46abad7f841ed8628c4a
Thallys Morais <kyotodevindie@gmail.com>
2023-12-10 21:11:32 -0300

    feat: cypress login test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,5 @@
 describe('Login Test', () => {
-  it('should log in with valid credentials', () => {
+  it('should log in and log out with valid credentials', () => {
     cy.visit(Cypress.env('baseUrl'))
 
     cy.get('[data-testid="cypress-title-signin"]').should(
@@ -7,28 +7,26 @@ describe('Login Test', () => {
       'Sign in to your account',
     )
 
-    // Preencher o formulário de login
     cy.get('input[name="username"]').type(Cypress.env('username'))
     cy.get('input[name="password"]').type(Cypress.env('password'))
 
-    // Clicar no botão de login
     cy.get('button[type="submit"]').click()
 
-    // Verificar se o usuário foi redirecionado para a página após o login
     cy.url().should('include', '/user')
 
-    // Verificar se o usuário está logado
     cy.get('[data-testid="cypress-title-userInfo"]').should(
       'contain',
       'User Info',
     )
+
+    cy.get('[data-testid="cypress-link-signout"]').click()
+
+    cy.url().should('include', '/')
   })
 
   it('should display an error message with invalid credentials', () => {
-    // Visitar a página de login
     cy.visit(Cypress.env('baseUrl'))
 
-    // Preencher o formulário de login com credenciais inválidas
     cy.get('input[name="username"]').type('invalid_username')
     cy.get('input[name="password"]').type('invalid_password')
 
@@ -37,10 +35,8 @@ describe('Login Test', () => {
       'Sign in to your account',
     )
 
-    // Clicar no botão de login
     cy.get('button[type="submit"]').click()
 
-    // Verificar se a mensagem de erro é exibida
-    // cy.get('.error-message').should('be.visible')
+    cy.get('.Toastify__toast-container').should('be.visible')
   })
 })
